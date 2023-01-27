7f8bab5d666ceeac809ec1e74ba5fe99615e0d85
Miguel Cardoso <45210483+miguelangelooscardoso@users.noreply.github.com>
2023-01-27 17:46:17 +0000

    Delete assets/js directory

--- assets/js/dark-mode.js
+++
@@ -1,5 +0,0 @@
-if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
-  $('.dark-mode').addClass('dark-mode-active');
-} else {
-  $('.dark-mode').removeClass('dark-mode-active');
-}
